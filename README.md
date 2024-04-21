5ebe400a0e1e9b0d5404b516c686ebe8e8aa02d8
CaoKiet251 <caoquockiet04@gmail.com>
2024-04-21 09:57:22 +0700

    add profileuser and updateprofile

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # SharingRecipeApplication
-ahcjy
--- app/src/main/java/com/example/sharingrecipeapp/Activities/UserActivity.java
+++ app/src/main/java/com/example/sharingrecipeapp/Activities/UserActivity.java
@@ -1,55 +1,48 @@
 package com.example.sharingrecipeapp.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.sharingrecipeapp.R;
+import com.example.sharingrecipeapp.UpdateProfileActivity;
 
 public class UserActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
-        setupToolbar();
-    }
 
-
-    private void setupToolbar(){
-        toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        MenuInflater inflater = getMenuInflater();
-//        inflater.inflate(R.menu.setting_menu,menu);
-//        return true;
-//    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.setting_menu,menu);
         return  true;
     }
-
-//    @Override
-//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-//        if (item.getItemId() == R.id.update) {
-//
-//        } else if (item.getItemId() == R.id.display) {
-//            Toast.makeText(this, "you click display", Toast.LENGTH_LONG).show();
-//        } else if (item.getItemId() == R.id.language) {
-//            Toast.makeText(this, "you click language", Toast.LENGTH_LONG).show();
-//        }else if(item.getItemId()==R.id.logout) {
-//            Toast.makeText(this, "you click logout", Toast.LENGTH_LONG).show();
-//        }
-//        return true;
-//    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.update) {
+            Intent update = new Intent(UserActivity.this, UpdateProfileActivity.class);
+            startActivity(update);
+        } else if (item.getItemId() == R.id.display) {
+            Toast.makeText(this, "you click display", Toast.LENGTH_LONG).show();
+        } else if (item.getItemId() == R.id.language) {
+            Toast.makeText(this, "you click language", Toast.LENGTH_LONG).show();
+        }else if(item.getItemId()==R.id.logout) {
+            Toast.makeText(this, "you click logout", Toast.LENGTH_LONG).show();
+        }
+        return true;
+    }
 }
