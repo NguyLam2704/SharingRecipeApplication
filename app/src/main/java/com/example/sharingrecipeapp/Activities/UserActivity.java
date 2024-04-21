26fa9696d3789f138b8c8f6d8b6f629e989122f6
CaoKiet251 <caoquockiet04@gmail.com>
2024-04-21 14:09:36 +0700

    add logout account

--- app/src/main/java/com/example/sharingrecipeapp/Activities/UserActivity.java
+++ app/src/main/java/com/example/sharingrecipeapp/Activities/UserActivity.java
@@ -1,9 +1,11 @@
 package com.example.sharingrecipeapp.Activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -41,8 +43,28 @@ public class UserActivity extends AppCompatActivity {
         } else if (item.getItemId() == R.id.language) {
             Toast.makeText(this, "you click language", Toast.LENGTH_LONG).show();
         }else if(item.getItemId()==R.id.logout) {
-            Toast.makeText(this, "you click logout", Toast.LENGTH_LONG).show();
+            logoutAccount(UserActivity.this);
         }
         return true;
+    }
+    private void logoutAccount(UserActivity userActivity){
+        AlertDialog.Builder builder=new AlertDialog.Builder(userActivity);
+        builder.setTitle("Đăng xuất");
+        builder.setMessage("Bạn có chắc chắn muốn đăng xuất không?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent=new Intent(UserActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
     }
 }
