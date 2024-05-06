1e133dc680bcbe9d6fd22c3be37ca7668169308d
NgPhuong <Phuongngusee@gmail.com>
2024-05-06 11:41:52 +0700

    Them mon an vao PlanList tu SavedRecipe, fix loi swipe de xoa mon an

--- app/src/main/java/com/example/sharingrecipeapp/Activities/PlantoSavedActivity.java
+++ app/src/main/java/com/example/sharingrecipeapp/Activities/PlantoSavedActivity.java
@@ -3,13 +3,16 @@ package com.example.sharingrecipeapp.Activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -21,13 +24,20 @@ import com.example.sharingrecipeapp.Classes.Recipes;
 import com.example.sharingrecipeapp.Fragments.PlanFragment;
 import com.example.sharingrecipeapp.R;
 import com.example.sharingrecipeapp.databinding.ActivityPlantoSavedBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PlantoSavedActivity extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -36,12 +46,16 @@ public class PlantoSavedActivity extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseFirestore db;
 
+    PlanFragment planFragment;
+
     ImageButton back_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planto_saved);
         binding = ActivityPlantoSavedBinding.inflate(getLayoutInflater());
+
+
 
 
         recyclerView = findViewById(R.id.List_save_recipes);
@@ -60,47 +74,78 @@ public class PlantoSavedActivity extends AppCompatActivity {
 
     }
 
-    private void displaySavedRecipes() {
-        List<String> tenRecipe = new ArrayList<>();
-        db.collection("SaveRecipes").whereEqualTo("idUser",auth.getUid()).get()
+    private void displaySavedRecipes(){
+        db.collection("SaveRecipes").whereArrayContains("idUsers",auth.getUid()).get()
                 .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        for (DocumentSnapshot doc : task.getResult()) {
-                            tenRecipe.add(doc.get("idRecipes").toString());
-                        }
-                        ArrayList<Recipes> recipesList = new ArrayList<>();
-                        for (String i : tenRecipe) {
-                            db.collection("Recipes").document(i).get()
-                                    .addOnSuccessListener(documentSnapshot -> {
-                                        String image = documentSnapshot.getString("image");
-                                        String name = documentSnapshot.getString("name");
-                                        String save = documentSnapshot.get("save").toString();
-                                        String time = documentSnapshot.get("timecook").toString();
+                   if (task.isSuccessful()){
+                       List<String> tenRecipes = new ArrayList<>();
+                       for (DocumentSnapshot documentSnapshot : task.getResult().getDocuments()){
+                           tenRecipes.add(documentSnapshot.get("Recipes").toString());
+                       }
+                       ArrayList<Recipes> recipesList = new ArrayList<>();
+                       for (String i : tenRecipes){
+                           db.collection("Recipes").document(i).get()
+                                   .addOnSuccessListener(documentSnapshot -> {
+                                       String image = documentSnapshot.getString("image");
+                                       String name = documentSnapshot.getString("name");
+                                       String save = documentSnapshot.get("save").toString();
+                                       String time = documentSnapshot.get("timecook").toString();
 
-                                        Recipes recipes = new Recipes(i, image, name, save, time);
-                                        recipesList.add(recipes);
+                                       Recipes recipes = new Recipes(i,image,name,save,time);
+                                       recipesList.add(recipes);
 
-                                        RecipesAdapter myAdapter = new RecipesAdapter();
-                                        myAdapter.setData(recipesList, new IClickOnItemRecipe() {
-                                            @Override
-                                            public void onClickItemRecipe(Recipes recipes) {
-                                                onClickGoToDetailFood(recipes);
-                                            }
-                                        });
-                                        recyclerView.setLayoutManager(new GridLayoutManager(binding.getRoot().getContext(), 2));
-                                        recyclerView.setAdapter(myAdapter);
-                                    });
-                        }
-                    }
+                                       if (i == tenRecipes.get(tenRecipes.size()-1)){
+                                           RecipesAdapter myAdapter = new RecipesAdapter();
+                                           myAdapter.setData(recipesList, new IClickOnItemRecipe() {
+                                               @Override
+                                               public void onClickItemRecipe(Recipes recipes) {
+                                                   onClickGoToDetailFood(recipes);
+                                               }
+                                           });
+                                           recyclerView.setLayoutManager(new GridLayoutManager(binding.getRoot().getContext(), 2));
+                                           recyclerView.setAdapter(myAdapter);
+                                       }
+
+                                   });
+                       }
+                   }
                 });
-
     }
 
 
     private void onClickGoToDetailFood(Recipes recipes) {
-        Intent intent = new Intent(binding.getRoot().getContext(),FoodDetailActivity.class);
-        intent.putExtra("id", recipes.getId());
-        startActivity(intent);
+        Bundle extras = getIntent().getExtras();
+        String date = extras.getString("date");
+        String weekID = extras.getString("weekID");
+        ArrayList<String> tenRecipe = new ArrayList<>();
+        tenRecipe.add(recipes.getId());
+
+
+        DocumentReference documentReference = db.collection("PlanList").document(auth.getUid()).collection(weekID).document(date);
+
+        documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()){
+                    documentReference.update("recipes",FieldValue.arrayUnion(recipes.getId()    ));
+                }else {
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("recipes", tenRecipe);
+                    documentReference.set(data);
+                }
+            }
+        });
+
+
+
+        Toast.makeText(binding.getRoot().getContext(),"Dang cap nhat",Toast.LENGTH_SHORT).show();
+        Intent turnBack = new Intent();
+        int weekOfYear = extras.getInt("weekOfYear");
+        turnBack.putExtra("weekOfYear",weekOfYear);
+        setResult(123,turnBack);
+//        setResult(123);
+        finish();
+
     }
 
 
