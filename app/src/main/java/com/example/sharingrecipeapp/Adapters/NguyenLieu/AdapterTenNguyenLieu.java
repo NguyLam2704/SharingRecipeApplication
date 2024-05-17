6e106662590d9153e3792501760af0b4b2bce1b9
NgPhuong <Phuongngusee@gmail.com>
2024-05-18 03:07:55 +0700

    Them duoc nguyen lieu, Hien thi duoc nguyen lieu da them

--- app/src/main/java/com/example/sharingrecipeapp/Adapters/NguyenLieu/AdapterTenNguyenLieu.java
+++ app/src/main/java/com/example/sharingrecipeapp/Adapters/NguyenLieu/AdapterTenNguyenLieu.java
@@ -5,8 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -16,21 +18,34 @@ import com.example.sharingrecipeapp.Adapters.Home.IClickOnItemRecipe;
 import com.example.sharingrecipeapp.Classes.NguyenLieu;
 import com.example.sharingrecipeapp.Classes.Recipes;
 import com.example.sharingrecipeapp.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.squareup.picasso.Picasso;
 
 import org.checkerframework.checker.units.qual.N;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class AdapterTenNguyenLieu extends RecyclerView.Adapter<TenNguyenLieuViewHolder> {
 
+    Context context;
+    FirebaseFirestore db;
+    FirebaseAuth auth;
     List<NguyenLieu> tenNL;
-    public List<NguyenLieu> addNL;
+
+    public AdapterTenNguyenLieu(Context context, List<NguyenLieu> tenNL) {
+        this.context = context;
+        this.tenNL = tenNL;
+    }
 
     public AdapterTenNguyenLieu() {
         tenNL = new ArrayList<>();
-        addNL = new ArrayList<>();
     }
 
     public void setData(List<NguyenLieu> list){
@@ -48,12 +63,31 @@ public class AdapterTenNguyenLieu extends RecyclerView.Adapter<TenNguyenLieuView
     public void onBindViewHolder(@NonNull TenNguyenLieuViewHolder holder, int position) {
         NguyenLieu nguyenLieu = tenNL.get(position);
         if (nguyenLieu != null){
-            holder.textView.setText(nguyenLieu.getName());
+            holder.tenNL.setText(nguyenLieu.getName());
+            holder.donvi.setText(nguyenLieu.getDonvi());
             Picasso.get().load(nguyenLieu.getImg()).into(holder.imageView);
             holder.view.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    addNL.add(nguyenLieu);
+                    EditText sl = holder.editText;
+                    db = FirebaseFirestore.getInstance();
+                    auth = FirebaseAuth.getInstance();
+                    if (!sl.getText().toString().equals("")){
+                        Map<String, Object> data = new HashMap<>();
+                        data.put("name",nguyenLieu.getName());
+                        data.put("donvi",nguyenLieu.getDonvi());
+                        data.put("idUser",auth.getUid());
+                        data.put("img",nguyenLieu.getImg());
+                        data.put("SL",Integer.valueOf(sl.getText().toString()));
+                        db.collection("ListNguyenLieuMua").add(data).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(context,"Đã thêm nguyên liệu", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }else {
+                        Toast.makeText(context,"Bạn cần thêm số lượng", Toast.LENGTH_SHORT).show();
+                    }
                 }
             });
         }
