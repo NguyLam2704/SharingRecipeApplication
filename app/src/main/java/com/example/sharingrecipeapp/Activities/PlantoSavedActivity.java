06b7d173eed4571a312c1720a62b34b34bcae06f
mluanUIT <22520827@gm.uit.edu.vn>
2024-05-19 16:13:37 +0700

    fix hien thi save

--- app/src/main/java/com/example/sharingrecipeapp/Activities/PlantoSavedActivity.java
+++ app/src/main/java/com/example/sharingrecipeapp/Activities/PlantoSavedActivity.java
@@ -3,6 +3,7 @@ package com.example.sharingrecipeapp.Activities;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -14,8 +15,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -39,10 +42,12 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class PlantoSavedActivity extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -52,9 +57,13 @@ public class PlantoSavedActivity extends AppCompatActivity {
     FirebaseFirestore db;
     TextView soluong;
     Integer number = 0;
+    List <Recipes> ResultSearchList;
 
     List<Recipes> recipesList;
 
+    ProgressBar Prgrss_save;
+
+    SearchView save_searchbar;
     ImageButton back_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,6 +78,33 @@ public class PlantoSavedActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         back_btn = findViewById(R.id.btn_back_profile);
         soluong = findViewById(R.id.textTb);
+        save_searchbar = findViewById(R.id.Searchbar);
+        Prgrss_save = findViewById(R.id.prgrss_save);
+
+        save_searchbar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+//                ResultSearchList.clear();
+                save_searchbar.setBackgroundResource(R.drawable.edittext_bound);
+                Prgrss_save.setVisibility(View.VISIBLE);
+                save_searchName(query);
+                return true;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+//                save_searchName(newText);
+//                ResultSearchList.clear();
+                save_searchbar.setBackgroundResource(R.drawable.query_bound);
+                if(newText.equals(""))
+                {
+                    save_searchbar.setBackgroundResource(R.drawable.edittext_bound);
+//                    recipesList.clear();
+                    displaySavedRecipes();
+                }
+                return true;
+            }
+        });
 
         displaySavedRecipes();
 
@@ -179,8 +215,115 @@ public class PlantoSavedActivity extends AppCompatActivity {
         setResult(123,turnBack);
 //        setResult(123);
         finish();
+    }
+
+
+    private void save_searchName(String newtext)
+    {
+        ResultSearchList = new ArrayList<>();
+        db.collection("SaveRecipes").whereArrayContains("idUsers",auth.getUid()).addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error != null) {
+                    Log.w("Error", "listen:error", error);
+                }
+
+                //lấy dữ liệu từ firebase
+                List<String> tenRecipes = new ArrayList<>();
+                for (DocumentSnapshot documentSnapshot : value.getDocuments()){
+                    tenRecipes.add(documentSnapshot.get("Recipes").toString());
+                }
+
+                for (String nameRecipe : tenRecipes){
+                    db.collection("Recipes").document(nameRecipe).get()
+                            .addOnSuccessListener(documentSnapshot -> {
+                                String image = documentSnapshot.getString("image");
+                                String name = documentSnapshot.getString("name");
+                                String time = documentSnapshot.get("timecook").toString();
+
+                                db.collection("SaveRecipes").whereEqualTo("Recipes",nameRecipe).addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                                        ArrayList<String> idUser = new ArrayList<>();
+                                        String save;
+                                        for (QueryDocumentSnapshot doc :value) {
+                                            if (doc.get("idUsers") != null) {
+                                                idUser = (ArrayList<String>) doc.get("idUsers");
+                                            }
+                                            save = String.valueOf(idUser.size());
+                                            Recipes Newrcp = new Recipes(nameRecipe, image, name, save, time);
+                                            if(unAccent(Newrcp.getName().replace(" ","")).toLowerCase().contains(unAccent(newtext.toLowerCase().replace(" ",""))))
+                                            {
+                                                ResultSearchList.add(Newrcp);
+                                            }
+                                            if(ResultSearchList.isEmpty()) {
+                                                soluong.setText("Không có kết quả phù hợp");
+                                            }
+                                            else{
+                                                //tạm
+                                                soluong.setText("Có "+ResultSearchList.size()+" kết quả phù hợp");
+                                            }
+                                            RecipesAdapter myAdapter = new RecipesAdapter();
+                                            myAdapter.setData(ResultSearchList,new IClickOnItemRecipe() {
+                                                @Override
+                                                public void onClickItemRecipe(Recipes recipes) {
+                                                    onClickGoToDetailFood(recipes);
+                                                }
+                                            });
+                                            recyclerView.setAdapter(myAdapter);
+                                        }
+                                    }
+                                });
+//                    Explore_listRecipes_suggest = new ArrayList<>();// tim lai danh sach, dieu kien có luot save lon
+//                    save_db.collection("SaveRecipes").whereArrayContains("idUsers",auth.getUid())
+//                            //.whereGreaterThanOrEqualTo("Save",2)
+//                            .addSnapshotListener(new EventListener<QuerySnapshot>() {
+//                                @Override
+//                                public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+//                                    if (error != null) {
+//                                        Log.w("Error", "listen:error", error);
+//                                    }
+//                                    //lấy dữ liệu từ firebase
+//                                    for (DocumentSnapshot documentSnapshot : value.getDocuments()){
+//                                        String id = documentSnapshot.getString("id");
+//                                        String image = documentSnapshot.getString("image");
+//                                        String name = documentSnapshot.getString("name");
+//                                        String save = String.valueOf(documentSnapshot.get("save"));
+//                                        String time = documentSnapshot.getString("timecook");
+//
+//                                        Explore_listRecipes_suggest.add(new Recipes(id, image, name, save, time));
+//                                    }
+//                                }
+//                            });
+//                    Explore_adapter.setData(Explore_listRecipes_suggest,new IClickOnItemRecipe() {
+//                        @Override
+//                        public void onClickItemRecipe(Recipes recipes) {
+//                            onClickGoToDetailFood(recipes);
+//                        }
+//                    });
+//                    txtIngredients.setText("Món ăn bạn tìm đang được cập nhật\nMột số món gợi ý");
+//                    Explore_recyclerViewRandom.setAdapter(Explore_adapter);
+                            });
+
+                }
+                //search ko co ket qua
+            }
+
+        });
+        Prgrss_save.setVisibility(View.GONE);
+    }
+
+    public static String unAccent(String s) {
+        String temp = Normalizer.normalize(s, Normalizer.Form.NFD);
+        Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+        if (s.equals("Đ") || s.equals("đ"))
+        {
+            return pattern.matcher(temp).replaceAll("").replaceAll("Đ", "D").replace("đ", "d");
+        }
+        return pattern.matcher(temp).replaceAll("").replace('đ','d').replace('Đ','D');
 
     }
+
 
 
 }
